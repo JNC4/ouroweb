6ff056cbfce4b109e8199af6bd33685cf19627b8
JNC4 <137045298+JNC4@users.noreply.github.com>
2024-03-08 10:20:17 +0000

    Update ouroweb.py

--- ouroweb.py
+++ ouroweb.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, send_from_directory
 import time
+import os
 
 app = Flask(__name__)
 
 # This should be replaced with a more secure way to store passwords
 users = {'Ouroboros': 'Snakey'}
+
+@app.route('/ouroboros/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def home():
@@ -33,6 +38,19 @@ def pong():
 @app.route('/crowd')
 def crowd():
     return render_template('crowd.html')
+
+@app.route('/api/images', methods=['GET'])
+def get_image_filenames():
+    image_dir = 'C:/Users/jnc4m/directories/ouroboros/static/zerepic6m'
+    try:
+        filenames = os.listdir(image_dir)
+        return jsonify(filenames)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/message')
+def message():
+    return render_template('message.html')
 
 @app.route('/tower')
 def tower():
